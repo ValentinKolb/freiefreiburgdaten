1e4151475c63b0a52164e6ae6ddcd1ebd43e5c69
Valentin Kolb <valentinolb@uni-ulm.de>
2021-06-18 16:48:03 +0200

    added map and click handler on map

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from urllib.request import urlopen
 import plotly.express as px
+import json
 import plotly.graph_objects as go
 
 import dash
@@ -9,6 +11,9 @@ from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+with open("/Users/valentinkolb/Git/freiefreiburgdaten/assets/.mapbox_accesstoken") as access_token:
+    mapbox_access_token = access_token.read()
 
 
 ##
@@ -49,6 +54,38 @@ df_number_students = pd.read_csv("/Users/valentinkolb/Git/freiefreiburgdaten/dat
 df_number_doctorate = pd.read_csv("/Users/valentinkolb/Git/freiefreiburgdaten/data/number_doctorate.csv")
 df_number_graduate = pd.read_csv("/Users/valentinkolb/Git/freiefreiburgdaten/data/number_graduates.csv")
 
+df = pd.DataFrame()
+
+##
+# MAP
+##
+
+scatter = go.Scattermapbox(
+    lat=['47.9943809094517', '47.99352971943071'],
+    lon=['7.844843887429616', '7.846003242830979'],
+    mode='markers',
+    fillcolor="grey",
+    marker={'size': 20, 'symbol': ["library", "college"]},
+    text=["Universitätsbibliothek", "Universität"],
+)
+
+fig = go.Figure(scatter)
+
+fig.update_layout(
+    autosize=True,
+    hovermode='closest',
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(
+            lat=47.99352971943071,
+            lon=7.846003242830979
+        ),
+        pitch=3,
+        zoom=15
+    ),
+)
+
 ##
 # MAIN APP LAYOUT
 ##
@@ -57,20 +94,28 @@ app.layout = html.Div([
 
     # HEADER
     html.Div(
-        html.Img(src=app.get_asset_url('logo_freiburg.png'),
-                 style={'height': '80%', 'width': '80%', "display": "block", "margin": "0 auto"}),
-        style={'width': '10%', 'display': 'inline-block'}
-    ),
-    html.Div(
-        html.H1("Open-Data Freiburg"),
-        style={'display': 'inline-block', "vertical-align": "middle"}
+        [
+            html.Img(src=app.get_asset_url('logo_freiburg.png'),
+                     style={'display': 'table-cell', 'width': '80%', 'vertical-align': 'middle'}
+                     ),
+            html.H1("Open-Data Freiburg ... and more",
+                    style={'display': 'table-cell', 'vertical-align': 'middle', 'width': '95%'}
+                    )
+        ], style={"display": "table", 'padding': '10px 60px'}
     ),
 
-    html.Div([dcc.Graph(figure=px.choropleth_mapbox(data_frame=pd.DataFrame(),center={"lat": 48.00217366211979, "lon": 7.837318006483093},
-                                          mapbox_style="open-street-map",
-                                          zoom=8.5))]),
+    dcc.Graph(id='freiburg_map', figure=fig),
 
-    # UNIVERSITY DATA
+    # DATA
+    html.Div(id="data_area", children=[], style={'padding': '10px 60px'})
+
+])
+
+##
+# UNIVERSITY DATA
+##
+
+university_layout = [
     html.H2("Daten über die Universität Freiburg"),
 
     html.Div(children=
@@ -116,16 +161,31 @@ app.layout = html.Div([
                            style={'width': "100%"}
                            ),
               html.Div(id="no_graduate_table", children=[])
-              ], style={'width': '33%', 'display': 'inline-block', 'vertical-align': 'top'}),
-
-    # MAIN DATA AREA
-
-])
+              ], style={'width': '33%', 'display': 'inline-block', 'vertical-align': 'top'})
+]
 
 
 ##
 # INTERACTIVITY
 ##
+
+
+@app.callback(
+    Output('data_area', 'children'),
+    Input('freiburg_map', 'clickData')
+)
+def get_click_from_map(clickData):
+    if clickData:
+        location = clickData['points'][0]['text']
+
+        if location == 'Universitätsbibliothek':
+            print("click on Universitätsbibliothek")
+        elif location == 'Universität':
+            print('click on Universität')
+            return university_layout
+
+    return []
+
 
 @app.callback(
     [Output(component_id='no_students_table', component_property='children')],
